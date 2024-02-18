bd6b8e3b9a2ed13fb2f189d626e66cb52ce46cce
Carlos <2414186+0xCaponte@users.noreply.github.com>
2024-02-18 18:21:14 +0100

    Added rotation to the globe map

--- src/lib/components/GlobeMapComponent.svelte
+++ src/lib/components/GlobeMapComponent.svelte
@@ -1,31 +1,66 @@
 <script lang="ts">
-	import { geoGraticule, geoOrthographic, geoPath } from 'd3-geo';
+	import { geoOrthographic, geoPath } from 'd3-geo';
+	import { drag } from 'd3-drag';
+	import { select } from 'd3-selection';
 	import { selectedLanguages } from '$lib/store';
 	import type Language from '$lib/model/language';
 	import { MapHelper } from '$lib/helpers/MapHelper';
+	import { onMount } from 'svelte';
 
 	export let worldData: any;
 	export let languages: Language[];
 
 	// Map setup & rendering
-    let projection = geoOrthographic().scale(250).translate([480, 250]); 
+	let projection = geoOrthographic().scale(250).translate([480, 250]);
 	let path = geoPath().projection(projection);
-	let graticule = geoGraticule();
+	let rotation = [0, 0, 0]; // Initial rotation
 	let sphere = { type: 'Sphere' }; // Globe Outline
 
 	let countries = MapHelper.processCountries(worldData);
 	let borders = MapHelper.processBorders(worldData);
 
-	// Reactive code to update based on the languages
-	// Ensures updateCountries is called whenever there's a change in selectedLanguages
+	// Reactive code to update on map dragging
+	$: if (projection) {
+		projection.rotate(rotation);
+		path = geoPath().projection(projection); // Ensure path is updated here too
+	}
+
+	// Reactive code to update the map colors based on the languages
 	$: $selectedLanguages, MapHelper.updateCountries(countries, languages);
+
+	/**
+	 * Calculates the rotation effects when the user drags on the map
+	 *
+	 * @param dragEvent
+	 */
+	function onDrag(dragEvent: { dx: any; dy: any }) {
+		const dx = dragEvent.dx;
+		const dy = dragEvent.dy;
+		const currentRotation = projection.rotate();
+		const radius = projection.scale();
+		const scale = 360 / (2 * Math.PI * radius);
+
+		rotation = [
+			currentRotation[0] + dx * scale,
+			currentRotation[1] - dy * scale,
+			currentRotation[2]
+		];
+	}
+
+	/**
+	 * Sets-up the drag event for the SVG map
+	 */
+	onMount(() => {
+		const svg = select('svg');
+		svg.call(drag().on('drag', onDrag));
+	});
 </script>
 
 <svg width="100%" height="100%" viewBox="0 0 960 500" preserveAspectRatio="xMidYMid meet">
-	
-    <!-- Globe outline -->
+	<!-- Globe outline -->
 	<path d={path(sphere)} fill="none" stroke="#000" />
 
+	<!--Countries -->
 	{#each countries as country}
 		<path
 			d={path(country)}
@@ -33,5 +68,7 @@
 			stroke="#000"
 		/>
 	{/each}
+
+	<!--Borders -->
 	<path d={path(borders)} fill="none" stroke="#000" />
 </svg>
