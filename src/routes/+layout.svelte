6be4292147a32f1f58513fba9226863825c52969
Carlos <2414186+0xCaponte@users.noreply.github.com>
2024-03-01 11:21:34 +0100

    Added Ko-Fi support to the footer. The footer has now two versions depending of screen size.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -68,26 +68,85 @@
 		<slot />
 	</div>
 
-	<!-- Footer -->
-	<footer class="px-4 py-2 w-full" style="height: var(--footer-height);">
-		<div class="grid grid-cols-2 gap-4 lg:flex lg:flex-1 lg:gap-0">
-			<!-- Globe and All Rights Reserved -->
-			<div class="flex flex-row items-center justify-start col-span-1">
-				<a href="https://caponte.io" target="_blank" class="block">
-					<GlobeOutline size="lg" />
-				</a>
-				<p class="text-xs ml-2">© {currentYear} All Rights Reserved.</p>
+	<!-- Footers -->
+
+	<!-- Footer for larger screens ( >= LG)  -->
+	<footer class="hidden lg:flex px-4 py-2 w-full" style="height: var(--footer-height);">
+		<div class="flex flex-row justify-between items-center w-full">
+			<!-- KoFi Component on the left -->
+			<div class="mb-4 lg:mb-0">
+				<!-- Custom KoFi Button -->
+				<button on:click={() => window.open('https://ko-fi.com/caponte', '_blank')}>
+					<img
+						height="35"
+						style="border:0; border-radius:8px; height:35px; width: auto;"
+						src="https://storage.ko-fi.com/cdn/brandasset/kofi_button_stroke.png"
+						alt="Support me on Ko-fi"
+					/>
+				</button>
 			</div>
 
-			<!-- Twitter Icon -->
-			<div class="col-span-1 ml-auto">
+			<!-- Globe and Copyright centered for small screens, inline for larger -->
+			<div
+				class="flex flex-col lg:flex-row items-center justify-center lg:justify-between space-y-2 lg:space-y-0 lg:space-x-2"
+			>
+				<a href="https://caponte.io" target="_blank" class="block lg:mb-0">
+					<GlobeOutline size="lg" />
+				</a>
+				<p class="text-xs">© {currentYear} All Rights Reserved.</p>
+			</div>
+
+			<!-- Twitter Icon on the right -->
+			<div>
 				<a
 					href="https://x.com/0xcaponte"
 					target="_blank"
 					class="flex items-center font-semibold border shadow-md rounded-xl justify-center px-4 py-2 lg:justify-end"
+					style="height:35px; width: auto;"
 				>
 					<TwitterSolid size="md" /> @0xCaponte
 				</a>
+			</div>
+		</div>
+	</footer>
+
+	<!-- Footer for smaller screens ( < LG) -->
+	<footer class="lg:hidden px-4 py-2 w-full">
+		<div class="flex flex-col items-center space-y-2">
+			<!-- First Row: KoFi Button and Twitter Icon -->
+			<div class="flex justify-between items-center w-full">
+				<!-- Custom KoFi Button with different image and text "Support me" -->
+				<button
+					on:click={() => window.open('https://ko-fi.com/caponte', '_blank')}
+					class="flex items-center font-semibold border shadow-md rounded-xl justify-center px-2 py-2"
+					style="height:35px; width: 150px;"
+				>
+					<img
+						height="35"
+						style="border:0; border-radius:8px; height:35px; width: auto;"
+						src="https://storage.ko-fi.com/cdn/brandasset/kofi_s_logo_nolabel.png"
+						alt="Support me on Ko-fi"
+					/>
+					<span class="ml-1">Support me</span>
+				</button>
+
+				<!-- Twitter Icon -->
+				<a
+					href="https://x.com/0xcaponte"
+					target="_blank"
+					class="flex items-center font-semibold border shadow-md rounded-xl justify-center px-2 py-2"
+					style="height:35px; width: 150px;"
+				>
+					<TwitterSolid size="md" /> @0xCaponte
+				</a>
+			</div>
+
+			<!-- Second Row: Globe and Copyright Notice -->
+			<div class="flex justify-center items-center w-full">
+				<a href="https://caponte.io" target="_blank" class="block">
+					<GlobeOutline size="md" />
+				</a>
+				<p class="text-xs">© {currentYear} All Rights Reserved.</p>
 			</div>
 		</div>
 	</footer>
