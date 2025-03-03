5a8c7bd35dc51f489cf12cd75ec441ac6497c0ad
Carlos Aponte <2414186+0xCaponte@users.noreply.github.com>
2025-03-03 19:03:31 +0100

    Changed the sitemap so it has the canonical root-url

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -3,6 +3,6 @@ import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
   return await sitemap.response({
-    origin: 'https://languagemap.world',
+    origin: 'https://www.languagemap.world',
   });
 };
